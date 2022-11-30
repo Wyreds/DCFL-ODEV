632565616669eba80f79ec691d730b66795adab3
Wyreds <96042849+Wyreds@users.noreply.github.com>
2022-11-30 23:30:02 +0300

    Add files via upload

---
+++ DCFL/Dairenin Alanı Ve Çevresi.py
@@ -0,0 +1,7 @@
+#Dairenin Alanını Ve Çevresini Hazırlayan Program
+YariCap= float(input(" Dairenin Yarı Çapını Giriniz " ))
+DaireAlan= 3.14*(YariCap**2)
+DaireCevre= 3.14*(YariCap*2)
+print("Dairenin Alanı",DaireAlan)
+print("Dairenin Çevresi",DaireCevre)
+print( "Dairenizin Alanı Ve Cevresi Hesaplanmıştır")
---
+++ DCFL/Kupun Hacmi.py
@@ -0,0 +1,7 @@
+#Küpün Hacmini ve yüzey alanını hesaplayan Kod
+Kenar = float(input("Küpün Kenarını Giriniz: "))
+hacim = Kenar*(Kenar**2)
+YuzeyAlani =  6*(Kenar**2)
+print("Küpün Hacimi",hacim)
+print("Küpün Yuzey Alani",YuzeyAlani)
+print("Küpün Hacmi Ve Yüzey Alabı Hesaplanmıştır.")
